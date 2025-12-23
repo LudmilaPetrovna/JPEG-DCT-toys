5d79cd57c591c98e6a9464b101ef08c6508545ce
Ludmila Petrovna <LudmilaPetrovna777@hotmail.com>
2025-12-23 07:45:53 +0400

    sync

--- make_ref.pl
+++ make_ref.pl
@@ -13,13 +13,18 @@ $pic->alphaBlending(0);
 $pic->filledRectangle(0,0,8,8,0xFFFFFF);
 $pic->alphaBlending(1);
 
+my $output;
 $pic->stringFT(0x0,"/usr/share/fonts/truetype/Fifaks10Dev1.ttf",9,0,1,10,$symbol);
 treshold();
-write_file("ref/".$symbol."0.png",$pic->png(9));
+$output="ref/".$symbol."0.png";
+write_file($output,$pic->png(9));
+`optipng -O7 "$output"`;
 
 $pic->line(7,0,7,7,0x0);
 treshold();
-write_file("ref/".$symbol."1.png",$pic->png(9));
+$output="ref/".$symbol."1.png";
+write_file($output,$pic->png(9));
+`optipng -O7 "$output"`;
 
 
 sub treshold{
